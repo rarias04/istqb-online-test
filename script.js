0e7be3d91266ce2d657ec5000e35a2c0651aefef
rarias04 <161629600+rarias04@users.noreply.github.com>
2024-05-11 19:20:49 +0200

    Added Welcome Functionality on script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.write("Hello world");
+function print(message, id){
+    let paragraph = document.getElementById(id);
+    paragraph.innerText = message;
+}
+
+function welcomeMessage(){
+    let name = document.getElementById("welcome1").value;
+    let message = " Welcome " + name + " to ISTQB online Test";
+    print(message, "welcomeresponse");
+}
