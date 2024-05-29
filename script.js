2420b548aef3f0fe7b08b12ea6e9517e33c0ffd7
rarias04 <161629600+rarias04@users.noreply.github.com>
2024-05-29 10:03:24 +0200

    Changes made on the code for showing ten questions feature
    Changes made on the code for showing ten questions feature
    https://bootcampqarebeca.atlassian.net/browse/IOT-5

--- script.js
+++ script.js
@@ -9,383 +9,158 @@ function welcomeMessage(){
     print(message, "welcomeResponse");
 }
 
-function showAnswer1(){
-    let answer = "B";
-    let message = "The correct answer is " + answer;
-    print(message, "q1Response")
+const testAnswers =  ["B", "A", "B", "C", "A", "B", "A", "C", "C", "A"];
+const tagsNames = ["q1Response", "q2Response", "q3Response", "q4Response", "q5Response", "q6Response", "q7Response", "q8Response", "q9Response", "q10Response"];
+const tagsCheckAnswers = ["q1CheckAnswer", "q2CheckAnswer","q3CheckAnswer", "q4CheckAnswer", "q5CheckAnswer", "q6CheckAnswer", "q7CheckAnswer","q8CheckAnswer", "q9CheckAnswer", "q10CheckAnswer"];
+const correctCheckAnswer = "The correct answer is ";
+const emptyMessage = "Please, select an option. Zero points for an empty answer";
+const incorrectAnswerMessage = "The answer is incorrect, good try! One point is deducted for an incorrect answer";
+const correctAnswerMessage = "The answer is correct, well done! You get 2 points for a correct answer";
+
+
+function showAnswer() {
+
+    let answers = new Array(10);
+    let answer1= document.getElementById("options1").value;
+    answers[0] = answer1;
+    let answer2 = document.getElementById("options2").value;
+    answers[1] = answer2;
+    let answer3 = document.getElementById("options3").value;
+    answers[2] = answer3;
+    let answer4 = document.getElementById("options4").value;
+    answers[3] = answer4;
+    let answer5 = document.getElementById("options5").value;
+    answers[4] = answer5;
+    let answer6 = document.getElementById("options6").value;
+    answers[5] = answer6;
+    let answer7 = document.getElementById("options7").value;
+    answers[6] = answer7;
+    let answer8 = document.getElementById("options8").value;
+    answers[7] = answer8;
+    let answer9 = document.getElementById("options9").value;
+    answers [8] = answer9;
+    let answer10 = document.getElementById("options10").value;
+    answers[9] = answer10
+    
+    if (answers[0] !== ""){
+            print(correctCheckAnswer + testAnswers[0], tagsNames[0]);
+        }
+    
+    if (answers[1] !== ""){
+            print(correctCheckAnswer + testAnswers[1], tagsNames[1]);
+        }
+    
+    if (answers[2] !== ""){
+            print(correctCheckAnswer + testAnswers[2], tagsNames[2]);
+    }
+    if (answers[3] !== ""){
+            print(correctCheckAnswer + testAnswers[3], tagsNames[3]);
+    }
+    
+    if (answers[4] !== ""){
+            print(correctCheckAnswer + testAnswers[4], tagsNames[4]);
+    }
+    
+    if (answers[5] !== ""){
+            print(correctCheckAnswer + testAnswers[5], tagsNames[5]);
+    }
+    
+    if (answers[6] !== ""){
+            print(correctCheckAnswer + testAnswers[6], tagsNames[6]);
+    }
+    
+    if (answers[7] !== ""){
+            print(correctCheckAnswer + testAnswers[7], tagsNames[7]);
+    }
+    
+    if (answers[8] !== ""){
+            print(correctCheckAnswer + testAnswers[8], tagsNames[8]);
+    }
+    
+    if (answers[9] !== ""){
+            print(correctCheckAnswer + testAnswers[9], tagsNames[9]);
+    }
+    else {
+            print("error", "q11Response");
+    }
+}
+function checkAnswer(){
+
+    let answers = new Array(10);
+    let answer1= document.getElementById("options1").value;
+    answers[0] = answer1;
+    let answer2 = document.getElementById("options2").value;
+    answers[1] = answer2;
+    let answer3 = document.getElementById("options3").value;
+    answers[2] = answer3;
+    let answer4 = document.getElementById("options4").value;
+    answers[3] = answer4;
+    let answer5 = document.getElementById("options5").value;
+    answers[4] = answer5;
+    let answer6 = document.getElementById("options6").value;
+    answers[5] = answer6;
+    let answer7 = document.getElementById("options7").value;
+    answers[6] = answer7;
+    let answer8 = document.getElementById("options8").value;
+    answers[7] = answer8;
+    let answer9 = document.getElementById("options9").value;
+    answers [8] = answer9;
+    let answer10 = document.getElementById("options10").value;
+    answers[9] = answer10
+
+    if (answers[0] !== ""){
+        verifyAnswer(answers[0], 0);
+        
+    }
+    if (answers[1] !== ""){
+        verifyAnswer(answers[1], 1);
+        
+    }
+    if (answers[2] !== ""){
+        verifyAnswer(answers[2], 2);
+        
+    }   
+    if (answers[3] !== ""){
+        verifyAnswer(answers[3], 3);
+       
+    }
+    if (answers[4] !== ""){
+        verifyAnswer(answers[4], 4);
+        
+    }
+    if (answers[5] !== ""){
+        verifyAnswer(answers[5], 5);
+        
+    }
+    if (answers[6] !== ""){
+        verifyAnswer(answers[6], 6);
+        
+    }
+    if (answers[7] !== ""){
+        verifyAnswer(answers[7], 7);
+        
+    }
+    if (answers[8] !== ""){
+        verifyAnswer(answers[8], 8);
+        
+    }
+    if (answers[9] !== ""){
+        verifyAnswer(answers[9], 9);
+       
+    }
+    else {
+        print("error", "q11Response");
+    }
 }
 
-function checkAnswer1(){
-    let answer = document.getElementById("options1").value;
-    let message = "";
-    if (answer ==="B"){
-        message = "The answer is correct, well done!";
+function verifyAnswer(answer, index) {
+    if  (answer === testAnswers[index]) {
+        print(correctAnswerMessage, tagsCheckAnswers[index]);
     }
-    else if (answer === "A" || answer === "C") {
-        message = "The answer is incorrect, good try!";
+    else if (answer === "0") {
+        print(emptyMessage, tagsCheckAnswers[index]);
     }
-    else{
-        message = "Please, select an option";
+    else {
+        print(incorrectAnswerMessage, tagsCheckAnswers[index]);
     }
-    print(message, "q1CheckAnswer");
-
-}
-
-function seeScore1(){
-    let answer = document.getElementById("options1").value;
-    let message = "";
-    if (answer ==="B"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "A" || answer === "C") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q1seeScore");
-
-}
-
-function showAnswer2(){
-    let answer = "A";
-    let message = "The correct answer is " + answer;
-    print(message, "q2Response")
-}
-
-function checkAnswer2(){
-    let answer = document.getElementById("options2").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q2CheckAnswer");
-
-}
-
-function seeScore2(){
-    let answer = document.getElementById("options2").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q2seeScore");
-
-}
-
-function showAnswer3(){
-    let answer = "B";
-    let message = "The correct answer is " + answer;
-    print(message, "q3Response")
-}
-
-function checkAnswer3(){
-    let answer = document.getElementById("options3").value;
-    let message = "";
-    if (answer ==="B"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "A" || answer === "C") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q3CheckAnswer");
-
-}
-
-function seeScore3(){
-    let answer = document.getElementById("options3").value;
-    let message = "";
-    if (answer ==="B"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "A" || answer === "C") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q3seeScore");
-
-}
-
-function showAnswer4(){
-    let answer = "C";
-    let message = "The correct answer is " + answer;
-    print(message, "q4Response")
-}
-
-function checkAnswer4(){
-    let answer = document.getElementById("options4").value;
-    let message = "";
-    if (answer ==="C"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "A" || answer === "B") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q4CheckAnswer");
-
-}
-
-function seeScore4(){
-    let answer = document.getElementById("options4").value;
-    let message = "";
-    if (answer ==="C"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "A" || answer === "B") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q4seeScore");
-
-}
-
-function showAnswer5(){
-    let answer = "A";
-    let message = "The correct answer is " + answer;
-    print(message, "q5Response")
-}
-
-function checkAnswer5(){
-    let answer = document.getElementById("options5").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q5CheckAnswer");
-
-}
-
-function seeScore5(){
-    let answer = document.getElementById("options5").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q5seeScore");
-
-}
-
-function showAnswer6(){
-    let answer = "B";
-    let message = "The correct answer is " + answer;
-    print(message, "q6Response")
-}
-
-function checkAnswer6(){
-    let answer = document.getElementById("options6").value;
-    let message = "";
-    if (answer ==="B"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "A" || answer === "C") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q6CheckAnswer");
-
-}
-
-function seeScore6(){
-    let answer = document.getElementById("options6").value;
-    let message = "";
-    if (answer ==="B"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "A" || answer === "C") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q6seeScore");
-
-}
-
-
-function showAnswer7(){
-    let answer = "A";
-    let message = "The correct answer is " + answer;
-    print(message, "q7Response")
-}
-
-function checkAnswer7(){
-    let answer = document.getElementById("options7").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q7CheckAnswer");
-
-}
-
-function seeScore7(){
-    let answer = document.getElementById("options7").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q7seeScore");
-
-}
-
-function showAnswer8(){
-    let answer = "C";
-    let message = "The correct answer is " + answer;
-    print(message, "q8Response")
-}
-
-function checkAnswer8(){
-    let answer = document.getElementById("options8").value;
-    let message = "";
-    if (answer ==="C"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "A" || answer === "B") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q8CheckAnswer");
-
-}
-
-function seeScore8(){
-    let answer = document.getElementById("options8").value;
-    let message = "";
-    if (answer ==="C"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "A" || answer === "B") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q8seeScore");
-
-}
-
-function showAnswer9(){
-    let answer = "C";
-    let message = "The correct answer is " + answer;
-    print(message, "q9Response")
-}
-
-function checkAnswer9(){
-    let answer = document.getElementById("options9").value;
-    let message = "";
-    if (answer ==="C"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "A" || answer === "B") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q9CheckAnswer");
-
-}
-
-function seeScore9(){
-    let answer = document.getElementById("options9").value;
-    let message = "";
-    if (answer ==="C"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "A" || answer === "B") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q9seeScore");
-
-}
-
-function showAnswer10(){
-    let answer = "A";
-    let message = "The correct answer is " + answer;
-    print(message, "q10Response")
-}
-
-function checkAnswer10(){
-    let answer = document.getElementById("options10").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "The answer is correct, well done!";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "The answer is incorrect, good try!";
-    }
-    else{
-        message = "Please, select an option";
-    }
-    print(message, "q10CheckAnswer");
-
-}
-
-function seeScore10(){
-    let answer = document.getElementById("options10").value;
-    let message = "";
-    if (answer ==="A"){
-        message = "You get 2 points for a correct answer";
-    }
-    else if (answer === "B" || answer === "C") {
-        message = "One point is deducted for an incorrect answer";
-    }
-    else{
-        message = "Zero points for an empty answer";
-    }
-    print(message, "q10seeScore");
-
 }
